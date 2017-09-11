4eaa4a40b3d160bc65c56dab5df158bedb73580f
Tomasz Sabała <sabala.tomek@gmail.com>
2017-09-11 19:13:34 +0200

    Added flask restful. Plugged location tree export to a resource.

--- meerkat_consul/__init__.py
+++ meerkat_consul/__init__.py
@@ -2,25 +2,29 @@ import logging
 import os
 
 from flask import Flask
+from flask_restful import Api
 
 app = Flask(__name__)
+api = Api(app)
 
 from meerkat_consul.config import dhis2_config
 
-FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-handler = logging.StreamHandler()
-formatter = logging.Formatter(FORMAT)
-handler.setFormatter(formatter)
 logger = logging.getLogger("meerkat_consul")
-level_name = dhis2_config.get("loggingLevel", "ERROR")
-level = logging.getLevelName(level_name)
-logger.setLevel(level)
-logger.addHandler(handler)
+if not logger.handlers:
+    FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(FORMAT)
+    handler.setFormatter(formatter)
+    level_name = dhis2_config.get("loggingLevel", "ERROR")
+    level = logging.getLevelName(level_name)
+    logger.setLevel(level)
+    logger.addHandler(handler)
 
 api_url = os.environ.get('MEERKAT_API_URL', 'http://nginx/api')
 
+from meerkat_consul.export import ExportLocationTree
 
-
+api.add_resource(ExportLocationTree, "/exportLocationTree")
 
 @app.route('/')
 def root():
--- meerkat_consul/export.py
+++ meerkat_consul/export.py
@@ -2,6 +2,7 @@ import json
 
 from datetime import datetime
 import requests
+from flask_restful import abort, Resource
 
 from meerkat_consul import app, dhis2_config, logger, api_url
 from meerkat_consul.config import COUNTRY_LOCATION_ID, headers
@@ -12,96 +13,86 @@ __codes_to_ids = {}
 dhis2_api_url = dhis2_config["url"] + dhis2_config["apiResource"]
 dhis2_headers = dhis2_config["headers"]
 
-dhis2_ids = NewIdsProvider(dhis2_config["url"], dhis2_headers)
+dhis2_ids = NewIdsProvider(dhis2_api_url, dhis2_headers)
+
+COUNTRY_PARENT = 'ImspTQPwCqd'  # for testing with demo DHIS2 server, country should have no parent
 
 
-@app.route('/locations')
-def locations():
-    result = ""
-    location_tree = requests.get(api_url + "/locationtree", headers=headers)
-    country = location_tree.json()
-    country_details = get("{}/location/{!r}".format(api_url, COUNTRY_LOCATION_ID)).json()
-    if country_details["country_location_id"]:
-        dhis2_country_resp = get("{}/organisationUnits?filter=code:eq:{}".format(dhis2_api_url, country_details["country_location_id"])).json()
+class ExportLocationTree(Resource):
+    __codes_to_dhis2_ids = {}
+
+    def get(self):
+        location_tree = requests.get("{}/locationtree".format(api_url), headers=headers)
+        country = location_tree.json()
+        country_details = get("{}/location/{!r}".format(api_url, COUNTRY_LOCATION_ID)).json()
+
+        dhis2_organisation_code = country_details["country_location_id"]
+        __url = "{}/organisationUnits?filter=code:eq:{}".format(dhis2_api_url, dhis2_organisation_code)
+        dhis2_country_resp = get(__url, headers=dhis2_headers)
         dhis2_country_details = dhis2_country_resp.json().get("organisationUnits", [])
-        dhis2_parent_id = dhis2_country_details ["id"]
-    else:
-        # TODO: create country
-        dhis2_parent_id = dhis2_ids.pop()
-        pass
+        if dhis2_country_details:
+            self.__abort_if_more_than_one(dhis2_country_details, dhis2_organisation_code)
+            dhis2_parent_id = dhis2_country_details[0]["id"]
+        else:
+            dhis2_parent_id = self.__create_new_dhis2_organisation(country_details, COUNTRY_PARENT)
 
-    # TODO: check if county is there
-    locations = country["nodes"]
-    for location in locations:
-        loc_id = location["id"]
-        location_details = requests.get("{}/location/{!r}".format(api_url, loc_id)).json()
+        child_locations = country["nodes"]
+        self.__populate_child_locations(dhis2_parent_id, child_locations)
 
-        country_location_id = location_details["country_location_id"]
+        return 'ok'
+
+    @staticmethod
+    def __abort_if_more_than_one(dhis2_country_details, dhis2_organisation_code):
+        if len(dhis2_country_details) > 1:
+            logger.error("Received more than one organisation for given code: %s", dhis2_organisation_code)
+            abort(500)
+
+    @staticmethod
+    def __populate_child_locations(dhis2_parent_id, locations):
+        for location in locations:
+            loc_id = location["id"]
+            location_details = requests.get("{}/location/{!r}".format(api_url, loc_id)).json()
+
+            id = ExportLocationTree.__create_new_dhis2_organisation(location_details, dhis2_parent_id)
+            location_code = location_details["country_location_id"]
+            ExportLocationTree.__codes_to_dhis2_ids[location_code] = id
+
+            child_locations = location["nodes"]
+            ExportLocationTree.__populate_child_locations(id, child_locations)
+
+    @staticmethod
+    def __create_new_dhis2_organisation(location_details, dhis2_parent_id):
         name = location_details["name"]
+        country_location_id = location_details["country_location_id"]
+        # skip if organisation with given code already exists
+        dhis2_resp = get("{}organisationUnits?filter=code:eq:{}".format(dhis2_api_url, country_location_id),
+                         headers=dhis2_headers)
+        dhis2_organisation = dhis2_resp.json().get("organisationUnits", [])
+        if dhis2_organisation:
+            logger.info("Location %s with code %s already exists in dhis2.", name, country_location_id)
+            return dhis2_organisation[0]["id"]
+
+        uid = dhis2_ids.pop()
         if location_details["start_date"]:
-            # TODO: check this string > datetime > string formatting
-            opening_date = datetime.strptime(location_details["start_date"], "%Y-%m-%d %m:%s:%n").strftime("%Y-%m-%d")
+            opening_date = datetime.strptime(location_details["start_date"], "%Y-%m-%dT%H:%M:%S").strftime("%Y-%m-%d")
         else:
             opening_date = "1970-01-01"
         json_dict = {
-            "id": dhis2_ids.pop(),
+            "id": uid,
             "name": name,
             "shortName": name,
             "code": country_location_id,
             "openingDate": opening_date,
             "parent": {"id": dhis2_parent_id}
         }
+        payload = json.dumps(json_dict)
+        response = post("{}organisationUnits".format(dhis2_api_url), headers=dhis2_headers, data=payload)
+        logger.info("Created location %s with response %d", name, response.status_code)
+        logger.info(response.text)
+        return uid
 
-        print(json.dumps(location_details.json(), indent=4))
-        result += "{!r}: {!r}".format(location.get("id", "no_id"), location.get("text", "no_text"))
-        result += "\n"
+# example payload to be received from Meerkat Nest
 
-    return result
-
-
-def create_dhis2_organisation(_location):
-    """
-    Creates a dhis2 organisation.
-    :param _location: Meerkat location to be published as dhis2_organisation
-    :return: void
-    """
-    organisation_code = _location["country_location_id"]
-    if organisation_code is None:
-        return
-    if _location.start_date:
-        opening_date = _location.start_date.strftime("%Y-%m-%d")
-    else:
-        opening_date = "1970-01-01"
-    json_res = get("{}organisationUnits?filter=code:eq:{}".format(dhis2_api_url, organisation_code),
-                   header=dhis2_headers).json()
-    if not json_res['organisationUnits']:
-        __create_new_organisation(_location, opening_date, organisation_code)
-    else:
-        logger.info("Organisation %12s with code %15s already exists", _location.name, organisation_code)
-        uid = json_res['organisationUnits'][0]['id']
-        __codes_to_ids[organisation_code] = uid
-
-def __create_new_organisation(_location, opening_date, organisation_code):
-    uid = dhis2_ids.pop()
-    name = _location.name
-    parent_location_id = _location.parent_location
-    if parent_location_id == 1:
-        parent_id = dhis2_config['countryId']
-    else:
-        parent_id = __codes_to_ids[locations[parent_location_id].country_location_id]
-    json_dict = {
-        "id": uid,
-        "name": name,
-        "shortName": name,
-        "code": organisation_code,
-        "openingDate": opening_date,
-        "parent": {"id": parent_id}
-    }
-    payload = json.dumps(json_dict)
-    response = post("{}organisationUnits".format(dhis2_api_url), headers=dhis2_headers, data=payload)
-    logger.info("Created location %s with response %d", name, response.status_code)
-    logger.info(response.text)
-    __codes_to_ids[organisation_code] = uid
 
 upload_payload = {"token": "", "content": "record", "formId": "jor_evaluation", "formVersion": "",
                   "data": {"*meta-instance-id*": "uuid:32751c98-8390-4fa0-b0ed-1392d1ece3bc",
